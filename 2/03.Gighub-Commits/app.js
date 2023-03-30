b5a6bbc5b88d76280cdfe4f4eaad3642dee50098
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-30 11:49:10 +0300

    Update app.js

--- 2/03.Gighub-Commits/app.js
+++ 2/03.Gighub-Commits/app.js
@@ -1,4 +1,30 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let list = document.getElementById("commits");
+    let username = document.getElementById("username");
+    let repos = document.getElementById("repo");
+
+    console.log(username);
+    console.log(repos);
+
+
+    let request = fetch(`https://api.github.com/repos/${username.value}/${repos.value}/commits`)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data[0].commit.author.name);
+                list.innerHTML= "";
+
+                data.forEach(element => {
+                    let el = document.createElement("li");
+					let link = document.createElement("a");
+
+					link.href = element.html_url;
+					link.innerText = `${element.commit.author.name}: ${element.commit.message}`;
+
+					el.appendChild(link);
+					list.appendChild(el)
+                })
+            })
+
 }
