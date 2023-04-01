5d713c798f79c6666b84f85b81b56e2866c5c51f
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-04-01 19:26:06 +0300

    Update app.js

--- 02.Bus-Schedule/app.js
+++ 02.Bus-Schedule/app.js
@@ -1,11 +1,38 @@
 function solve() {
+    let arriveButt = document.getElementById("arrive");
+    let departButt = document.getElementById("depart");
+    let info = document.getElementById("info");
+    let id = "depot";
+    let stop = "";
 
     function depart() {
-        console.log('Depart TODO...');
-    }
+        departButt.disabled = true;
+        arriveButt.disabled = false;
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${id}`)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data.next);
+
+                info.innerText = `Next stop ${data.name}`;
+                stop = data.name;
+                id = data.next;
+            })
+    }   
 
     function arrive() {
-        console.log('Arrive TODO...');
+        departButt.disabled = false;
+        arriveButt.disabled = true;
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${id}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.next);
+
+            info.innerText = `Arrive at ${stop}`;
+        })
     }
 
     return {
